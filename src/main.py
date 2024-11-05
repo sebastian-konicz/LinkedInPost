e0e68261675654673bd3b79858602cd4b5b47508
unknown <sebastian.konicz@gmail.com>
2024-11-05 19:42:10 +0100

    Tweeking path to config

--- src/main.py
+++ src/main.py
@@ -1,14 +1,13 @@
+import sys
+import os
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from config.config import LINKEDIN_ACCESS_TOKEN
 from src.linkedin_client import LinkedInClient
 from config.secrets import get_access_token
 
 def main():
     access_token = LINKEDIN_ACCESS_TOKEN
-    if not access_token:
-        auth_code = input("Podaj kod autoryzacyjny LinkedIn: ")
-        access_token = get_access_token(auth_code)
-        print(f"Twój ACCESS_TOKEN: {access_token}")
-        # W realnej aplikacji zapisz token do pliku .env lub bazy danych.
 
     client = LinkedInClient(access_token)
     message = "Nowy post na LinkedIn wrzucony przy udziale GitHub Actions!"
