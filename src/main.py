b825a6b094353997067b4061a23a5bfd4fa96a85
unknown <sebastian.konicz@gmail.com>
2024-11-05 19:50:26 +0100

    Token tweeking

--- src/main.py
+++ src/main.py
@@ -1,14 +1,11 @@
-import sys
-import os
-sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
-from config.config import LINKEDIN_ACCESS_TOKEN
+# from config.config import LINKEDIN_ACCESS_TOKEN
 from src.linkedin_client import LinkedInClient
 from config.secrets import get_access_token
 
 def main():
     LINKEDIN_ACCESS_TOKEN = os.getenv("LINKEDIN_ACCESS_TOKEN")
     access_token = LINKEDIN_ACCESS_TOKEN
-    
+
     client = LinkedInClient(access_token)
     message = "Nowy post na LinkedIn wrzucony przy udziale GitHub Actions!"
     client.create_post(message)
